e6643f0f38da78751a15547d28dfe6f297fccf86
Gustav0Furtad0 <gufurtado02@gmail.com>
2023-10-27 14:27:31 -0300

    config target ts

--- api/routes/helloWorld.ts
+++ api/routes/helloWorld.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import { Request } from 'express';
 
-const router = express.Router();
+export const router = express.Router();
 
 router.get('/', (req:Request , res: { send: (arg0: string) => void; }) => {
     res.send('Olá, mundo!');
 });
-
-export { router };
